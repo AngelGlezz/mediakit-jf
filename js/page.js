51d2629975b826f4cf60d98ccf64ed99d69ee2bb
Angel <angelglezz.7@gmail.com>
2021-04-21 18:40:55 -0500

    update

--- js/page.js
+++ js/page.js
@@ -9,6 +9,15 @@ $(document).ready(function(){
 		slidesToScroll: 1,
 		arrows: false
 	});
+
+	/*for (var i = 1; i <= 7; i++) {
+		(function(index) {
+			setTimeout(function() {
+				$(".txt-animate").removeClass("be-bold");
+				$("#txt-video-"+index).addClass("be-bold");
+			}, 5000);
+		})(i);
+	}*/
 	
 	$(".nav-link").click(function(){
 		$('#navbarTogglerDemo02').removeClass('show');
@@ -45,88 +54,88 @@ $(document).ready(function(){
 	$('#audiencia').waypoint(function(){
 		$('#countdown').animateNumber(
 			{number:6001000}, 
-			6000,
+			2000,
 			function() {
 				$('#countdown').hide();
 				$('#txt-count').fadeIn();
 			}
 		);
-		$('#count-percentage').animateNumber(
-			{number:85}, 
-			3000,
+		/*$('#count-percentage').animateNumber(
+			{number:91}, 
+			2000,
 			function() {
 				$('#count-percentage').hide();
 				$('#percentage').fadeIn();
 			}
 		);
 		$('#count-millions').animateNumber(
-			{number:4600000}, 
-			4000,
+			{number:4500000}, 
+			2000,
 			function() {
 				$('#count-millions').hide();
 				$('#millions').fadeIn();
 			}
 		);
 		$('#count-miles').animateNumber(
-			{number:6001000}, 
-			4000,
+			{number:30000000}, 
+			2000,
 			function() {
 				$('#count-miles').hide();
 				$('#miles').fadeIn();
 			}
-		)}, {offset: '40%'}
+		)*/}, {offset: '40%'}
 	);
 
-	$('.title-quest').waypoint(function(){
+	/*$('.title-quest').waypoint(function(){
 		$('#cdmx').animateNumber(
-			{number:85}, 
-			3000,
+			{number:17}, 
+			2000,
 			function() {
 				$('#cdmx').hide();
 				$('#cdmx-final').fadeIn();
 			}
 		);
 		$('#mx').animateNumber(
-			{number:84}, 
-			3000,
+			{number:83}, 
+			2000,
 			function() {
 				$('#mx').hide();
 				$('#mx-final').fadeIn();
 			}
 		);
 		$('#women').animateNumber(
-			{number:83}, 
-			3000,
+			{number:25}, 
+			2000,
 			function() {
 				$('#women').hide();
 				$('#women-final').fadeIn();
 			}
 		);
 		$('#men').animateNumber(
-			{number:82}, 
-			3000,
+			{number:75}, 
+			2000,
 			function() {
 				$('#men').hide();
 				$('#men-final').fadeIn();
 			}
 		);
 		$('#young').animateNumber(
-			{number:81}, 
-			3000,
+			{number:79}, 
+			2000,
 			function() {
 				$('#young').hide();
 				$('#young-final').fadeIn();
 			}
 		);
 		$('#old').animateNumber(
-			{number:80}, 
-			3000,
+			{number:21}, 
+			2000,
 			function() {
 				$('#old').hide();
 				$('#old-final').fadeIn();
 			}
 		)}, {offset: '40%'}
-	);
+	);*/
 
 	$("#estrategia-item").click(function() {
 		$("#video-make-contenido").css('display', 'none');
