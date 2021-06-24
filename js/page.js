a1f7ad0d516b6a7dffd215dd1a3b3b787d3b4be9
Angel <angelglezz.7@gmail.com>
2021-06-24 13:36:50 -0500

    final test

--- js/page.js
+++ js/page.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+	jQuery.preloadImages = function(){
+		for(var i = 0; i<arguments.length; i++){
+			jQuery("<img>").attr("src", arguments[i]);
+			}
+		}
+
 	$('.social-video-link').fancybox();
 
 	$('.carousel').slick({
